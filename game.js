ccdf66d70ba193521f0f15db3097bf079767bb6f
AbSamad99 <abdussamad.mi5@gmail.com>
2019-08-27 21:51:14 +0530

    putting files up

---
+++ game.js
@@ -0,0 +1,73 @@
+var buttonColors=["red","blue","green","yellow"];
+var gamePattern=[];
+var userClickedPattern=[];
+var level=0;
+var started=false;
+
+$(".btn").click(function(){
+  var userChosenColor = $(this).attr("id");
+  userClickedPattern.push(userChosenColor);
+  console.log(userClickedPattern);
+  flash(userChosenColor);
+  playSound(userChosenColor);
+  animateFlash(userChosenColor);
+  checkAnswer(userClickedPattern.length-1);
+});
+
+$(document).keypress(function() {
+  if (!started) {
+    $("#level-title").text("Level " + level);
+    nextSequence();
+    started = true;
+  }
+});
+
+function flash(color){
+  $("#"+color).fadeOut(250).fadeIn(250);
+}
+
+function animateFlash(color){
+  $("#"+color).addClass("pressed");
+  setTimeout(function(){
+    $("#"+color).removeClass("pressed");
+  },100);
+}
+
+function playSound(color){
+  var audio=new Audio("sounds/"+color+".mp3");
+  audio.play();
+}
+
+function nextSequence(){
+  userClickedPattern=[];
+  level++;
+  $("#level-title").text("Level "+level);
+  var randomNumber=Math.floor(Math.random()*4);
+  var randomChosenColor = buttonColors[randomNumber];
+  gamePattern.push(randomChosenColor);
+  flash(randomChosenColor);
+  playSound(randomChosenColor);
+}
+
+function checkAnswer(currentLevel){
+  if(gamePattern[currentLevel]===userClickedPattern[currentLevel]){
+    if(gamePattern.length===userClickedPattern.length){
+      setTimeout(function(){nextSequence();},1000);
+    }
+  }
+  else{
+    playSound("wrong");
+    $("body").addClass("game-over");
+    $("#level-title").text("Game Over, Press Any Key To Restart");
+    setTimeout(function(){
+      $("body").removeClass("game-over");
+    },200);
+    startOver();
+  }
+}
+
+function startOver(){
+  level=0;
+  gamePattern=[];
+  started=false;
+}
